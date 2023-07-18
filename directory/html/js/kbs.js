0d9f00eb2d3267dd650b32a6e950d38ffaf682cf
kimbyoungsun <134191360+kimbyoungsun@users.noreply.github.com>
2023-07-18 17:37:18 +0900

    create: mainpage & hotplace
    create : mainpage & hotplace
    add: file
    2023-07-18 -kbs

---
+++ directory/html/js/kbs.js
@@ -0,0 +1,13 @@
+var mySwiper = new Swiper(".my-slider", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
